20a35c888a434350f03bf7d85bb522db0fe600fb
lhps1234 <lhps1234@gmail.com>
2023-08-11 12:38:42 +0900

    file upload not complete

---
+++ src/main/java/com/example/cyworld/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.example.cyworld.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${uploadPath}")
+    String uploadPath;
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+       
+// addResourceHandler("매핑 경로")를 적어둔다. localhost:8080/upload/ 로 들어오는 모든 정적 리소스 요청을 static폴더가 아닌 .addResourceLoactions에 적어둔 경로로 부터 찾아준다.
+registry.addResourceHandler("/miniHome/**").addResourceLocations(uploadPath);
+    }
+}
--- src/main/java/com/example/cyworld/controller/HomeController.java
+++ src/main/java/com/example/cyworld/controller/HomeController.java
@@ -7,6 +7,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.cyworld.config.PrincipalDetails;
 import com.example.cyworld.config.UserInfo;
@@ -28,4 +31,6 @@ public class HomeController {
     	model.addAttribute("adminUser", userInfo);
     	return "admin/index";
     }
+    
+   
 }
